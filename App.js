bed6d846a43bf1e9c935ed2a9deb6ab9d4810371
MOHAMMAD HAFEEZ <mohammad.hafeez@robosoftin.com>
2022-01-20 12:30:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>my app</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
